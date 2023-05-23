d40c2eb93925eb4dee7e580a3f4a7e86ece589e3
PoTrd <paultaurand@MacBook-Air-de-Paul.local>
2023-05-23 11:30:16 +0200

    add the about and the experience page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './core/contact/contact.component';
 import { MainComponent } from './core/main/main.component';
+import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'test', component: TestComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { WorkComponent } from './core/work/work.component';
 import { ContactComponent } from './core/contact/contact.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     ExperienceComponent,
     WorkComponent,
     ContactComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
