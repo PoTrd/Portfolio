b54a97f02535544a7bfbc0fe2d22d49f08b7ee5f
PoTrd <paul.taurand@gmail.com>
2024-11-19 13:49:59 +0100

    First version of the home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
 import styles from "../styles/Home.module.css";
-import Avatar from "@/components/avatar";
+import Avatar from "@/components/avatar/avatar";
+import Header from "@/components/header/header";
 
 export default function Home() {
   return (
       <div className={styles.container}>
-         <Avatar />
+          <Header />
+          <div className={styles.content}>
+              <div className={styles.sideStrip}>
+                <span>
+                    Software engineer
+                </span>
+                <hr/>
+                <span>
+                    {new Date().getFullYear().toString()}
+                </span>
+              </div>
+              <div className={styles.mainBloc}>
+                  <div className={styles.blocTitle}>
+                      <span className={styles.mainTitle}>
+                          Hi!
+                      </span>
+                      <span className={styles.secondaryTitle}>
+                        Welcome on my portfolio
+                      </span>
+                  </div>
+                <Avatar />
+              </div>
+          </div>
       </div>
   );
 }
