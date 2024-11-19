72ede30479955e686ba65b49f9fb168ab017519d
PoTrd <paul.taurand@gmail.com>
2024-11-19 13:49:34 +0100

    Update avatar

--- components/avatar/avatar.js
+++ components/avatar/avatar.js
@@ -1,13 +1,13 @@
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import { useEffect, useRef, useState } from "react";
+import styles from './avatar.module.css';
 
 function AvatarModel() {
     const { scene } = useGLTF('/RPM_avatar.glb');
     const headRef = useRef();
     const [targetRotation, setTargetRotation] = useState({ x: 0, y: 0 });
 
-    // Get the head reference from the scene
     const head = scene.getObjectByName('Head');
     if (head) {
         headRef.current = head;
@@ -38,25 +38,27 @@ function AvatarModel() {
 
 export default function Avatar() {
     return (
-        <Canvas
-            camera={{ position: [0, 1.5, 1], fov: 40}}
-        >
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[10, 10, 5]} intensity={1} />
-            <pointLight position={[-10, -10, -10]} intensity={0.5} />
-            <spotLight position={[5, 5, 5]} angle={0.3} penumbra={1} intensity={1} />
+        <div className={styles.canvaContainer}>
+            <Canvas
+                camera={{ position: [0, 1.5, 1], fov: 40, zoom: 1.25, }}
+            >
+                <ambientLight intensity={0.5} />
+                <directionalLight position={[10, 10, 5]} intensity={1} />
+                <pointLight position={[-10, -10, -10]} intensity={0.5} />
+                <spotLight position={[5, 5, 5]} angle={0.3} penumbra={1} intensity={1} />
 
-            <AvatarModel />
+                <AvatarModel />
 
-            <OrbitControls
-                target={[0, 1.65, 0]}
-                enablePan={false}
-                enableRotate={false}
-                enableZoom={false}
-                maxPolarAngle={Math.PI / 2}
-                minDistance={1}
-                maxDistance={3}
-            />
-        </Canvas>
+                <OrbitControls
+                    target={[0, 1.65, 0]}
+                    enablePan={false}
+                    enableRotate={false}
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minDistance={1}
+                    maxDistance={3}
+                />
+            </Canvas>
+        </div>
     );
 }
